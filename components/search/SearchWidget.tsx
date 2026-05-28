93c32abc363f3c8a91ae330426448aa54c7def3c
jishnukmtripzido <jishnu@tripzido.com>
2026-05-27 23:49:10 -0700

    modify home page header for mobile screen

--- components/search/SearchWidget.tsx
+++ components/search/SearchWidget.tsx
@@ -654,7 +654,7 @@ function TriggerButton({
           ? "border-red-300 hover:border-red-400"
           : active
             ? "border-[#ffc107] ring-2 ring-[#ffc107]/30"
-            : "border-gray-400 hover:border-[#ffc107]"
+            : "border-gray-300 hover:border-[#ffc107]"
         }`}
     >
       <div className="flex items-center min-w-0 flex-1">{children}</div>
