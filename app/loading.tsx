a0a1c5f1116c673e99577743dd77d2054d3c1087
jishnukmtripzido <jishnu@tripzido.com>
2026-05-31 15:42:11 -0700

    modify home page header for mobile screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/layout/Header";
 export default function Loading() {
   return (
     <>
-    <Header />
+     <Header logoWidth={4} logoHeight={4} logoTextSize="xl" linkIconsSize={4} userNameFirstLetterSize={8} userNameFirstLetter="J" headerLgScreenMx="xl:mx-[80.5px] xl:px-0" headerValues="w-full bg-white py-2 border-b border-gray-100 text-gray-900" />
     <div className="min-h-screen bg-white flex items-center justify-center">
       <div className="flex flex-col items-center gap-3">
         <div className="w-8 h-8 rounded-full border-2 border-[#ffc107] border-t-transparent animate-spin" />
