c0b4d289da393d0250e657495aabc90b36a5e7cc
jishnukmtripzido <jishnu@tripzido.com>
2026-07-11 19:18:00 +0530

    modify profile page

---
+++ app/booking-confirmed/layout.tsx
@@ -0,0 +1,22 @@
+import "../globals.css";
+import Header from "@/components/layout/Header";
+
+export default function BookingConfirmedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Header
+        logoWidth={4}
+        logoHeight={4}
+        logoTextSize="xl"
+        linkIconsSize={4}
+        headerLgScreenMx="xl:mx-[80.5px] xl:px-0"
+        headerValues=" w-full  py-2 border-b border-gray-100 text-gray-900 shadow-sm md:shadow-none"
+      />
+      {children}
+    </>
+  );
+}
